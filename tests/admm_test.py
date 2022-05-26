3a7abee60b9db2260db29f45fb16ac3a4c608c2f
greg-lund <greglund@stanford.edu>
2022-05-25 18:28:48 -0700

    >2 sensors in admm test

--- tests/admm_test.py
+++ tests/admm_test.py
@@ -54,50 +54,49 @@ def plot_state_traj(t,x,mu,cov,filename=None):
     else:
         plt.show()
 
-np.random.seed(1)
+#np.random.seed(1)
 dt = 1e-1
 tmax = 10
 t = np.arange(0,tmax,dt)
 n = 4
-m = 2
+
+# Sensor locations
+s = [np.array([0,10]).reshape(-1,1),np.array([10,0]).reshape(-1,1)]
+m = len(s)
 
 A = np.vstack([np.hstack([np.eye(2),dt*np.eye(2)]),np.hstack([np.zeros((2,2)),np.eye(2)])])
 Q = 0.1 * dt * np.eye(n)
 R = 0.1 * dt * np.eye(m)
 R_ind = 0.1 * dt * np.eye(1)
 
-C = np.hstack([np.vstack([np.eye(2),np.eye(2)]),np.zeros((4,2))])
-
 x0 = np.array([0,0,1,1]).reshape(-1,1)
-
-# Sensor locations
-s1 = np.array([0,10]).reshape(-1,1)
-s2 = np.array([10,0]).reshape(-1,1)
 
 x = np.zeros((len(t),n))
 y = np.zeros((len(t),m))
-meas = np.zeros((len(t),2,1))
+meas = np.zeros((len(t),m,1))
 x[0,:] = x0.T
 for i in range(len(t)-1):
     w = np.random.randn(n)
     xn = A @ x[i,:] + scipy.linalg.sqrtm(Q) @ w
     x[i+1,:] = xn.reshape(n,)
-    y[i+1,:] = np.array([rng_meas(s1,x[i+1,:]),rng_meas(s2,x[i+1,:])]) + scipy.linalg.sqrtm(R) @ np.random.randn(m)
-    meas[i,0,:] = y[i+1,0]
-    meas[i,1,:] = y[i+1,1]
+    y[i+1,:] = np.array([rng_meas(s[j],x[i+1,:]) for j in range(m)]) + scipy.linalg.sqrtm(R) @ np.random.randn(m)
+    for j in range(m):
+        meas[i,j,:] = y[i+1,j]
 
 mu0 = np.array([0,0,0,0]).reshape(-1,1)
 cov0 = np.eye(4)
 
-g = lambda x: np.vstack([rng_meas(s1,x),rng_meas(s2,x)])
-fC = lambda x: np.vstack([rng_jac(s1,x),rng_jac(s2,x)])
+g = lambda x: np.vstack([rng_meas(s[i],x) for i in range(m)])
+fC = lambda x: np.vstack([rng_jac(s[i],x) for i in range(m)])
 
-gs = [lambda x: rng_meas(s1,x),lambda x: rng_meas(s2,x)]
-fCs = [lambda x: rng_jac(s1,x),lambda x: rng_jac(s2,x)]
+gs = [lambda x,i=i: rng_meas(s[i],x) for i in range(m)]
+fCs = [lambda x,i=i: rng_jac(s[i],x) for i in range(m)]
 f_d = lambda x,t: A@x
 fA = lambda x,t: A
-neighbors = [[1],[0]]
 
+# Just have fully connected ADMM
+neighbors = [[j for j in [x for x in range(m) if x != i]] for i in range(m)]
+print("neighbors:",neighbors)
 
 print("Running ADMM")
 admm_est = ADMM_Estimator(gs,fCs,f_d,fA,neighbors,mu0,cov0,meas,Q,R_ind)
